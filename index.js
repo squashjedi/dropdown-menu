eebf80513ec017ee301cdedf336188cbf4653b15
squashjedi <paulandrewlord@gmail.com>
2022-09-07 13:31:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function dropdownMenu(options) {
+  document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-btn')
+
+    let currentContent
+    if (isDropdownButton) {
+      currentContent = e.target.nextElementSibling
+      currentContent.classList.toggle('active')
+    }
+
+    document.querySelectorAll('[data-dropdown]').forEach(dropdown => {
+      const dropdownContent = dropdown.querySelector('.content')
+      if (dropdownContent === currentContent) return
+      dropdownContent.classList.remove('active')
+    })
+  })
+}
+
+module.exports.dropdownMenu = dropdownMenu
